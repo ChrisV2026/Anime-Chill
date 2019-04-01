0c2bb72e8a98914183bbc095212877685321294c
Christopher Vishnu <vishnuc@mcmaster.ca>
2019-04-01 19:16:56 -0400

    Pop

--- src/DataAlloc/PopulateData.java
+++ src/DataAlloc/PopulateData.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Random;
 
+
 public class PopulateData {
 	public static void main(String[] args) throws IOException{
 		populateUsers();
