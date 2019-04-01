702519d07a76d483c0a8a47c5a72aeb20f7f2702
Christopher Vishnu <vishnuc@mcmaster.ca>
2019-04-01 19:05:48 -0400

    city

---
+++ src/DataAlloc/city.java
@@ -0,0 +1,8 @@
+package DataAlloc;
+
+public enum city {
+	boston, new_york_city, philadelphia, baltimore, washington, charlotte, atlanta, pittsburgh,
+	columbus, indianapolis, st_louis, kansas_city, denver, oklahoma_city, dallas, houston, new_orleans,
+	memphis, jacksonville, nashville, chicago, minneapolis, salt_lake_city, las_vegas, los_angeles,
+	san_francisco, portland, seattle, phoenix, albuquerque, san_antonio, miami;
+}
